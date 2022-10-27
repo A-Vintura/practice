f1d93a9f30f1dc4057f79288b90842c24af79fee
A-Vintura <116837502+A-Vintura@users.noreply.github.com>
2022-10-27 21:16:37 +0300

    Add files via upload

---
+++ knowledge.inc.php
@@ -0,0 +1,6 @@
+
+<?php
+$dogs_cat = 67;
+$stray_dogs_cat = 4.1;
+$percent_stray = $stray_dogs_cat * 100 / $dogs_cat;
+?>
---
+++ main.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Practice </title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    
+    <div class="flex-container">
+
+        <div class="header">     
+               <?php include 'logo.inc.php' ?>         
+               <?php include 'menu.inc.php' ?>	   
+        </div>       
+     
+        <div class="about_me">
+         
+          <h1>  <?php  echo $p  ?> </h1>
+
+            <div class="data">
+                <div class="myImg">
+                    <?php  echo '<img src="/img/foto.webp" width="200" height="300" alt="">'; ?>                    
+                </div>
+
+                <div class="fullname">
+                    <p> Меня зовут 
+                    <?php echo $name, ' ', $surname . '<br>'; 
+                          echo 'город', ' ', $city; ?>                                      
+                    </p> 
+           
+                    <p> Мне
+                    <?php  echo $age;   ?>          
+                    года </p>
+                    <p> Я найду вашего питомца </p>
+                </div>
+            </div>
+
+            <div class="knowledge">
+                                   
+                    <?php  include 'knowledge.inc.php'; ?>
+                    <?php   echo 'В России насчитывается ', $dogs_cat, ' ', 'млн кошек и собак. Из них ', $stray_dogs_cat, ' ', 'млн, или ', floor($percent_stray), '% ', '— бездомные.'; ?> <br>
+                                       
+                   <?php // $a = прошу прощения, что так мало использовал функций, мне катострофически не хватает времени?> 
+
+            </div>
+
+            <div class="article">
+                <p class="text">
+                    A cane non magno saepe ingens aper tenetur. <br>
+                    A majore bovi discit arare minor. <br>
+                    Ab abusu ad usum non valet consequentia.
+                </p>
+            </div>
+        </div>
+
+            <?php include 'footer.inc.php' ?>
+
+    </div>
+
+
+</body>
+</html>
